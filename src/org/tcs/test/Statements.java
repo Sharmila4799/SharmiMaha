da6d883655584ab2bc85854bf14ecd584a3d1dfe
Sharmila4799 <sharmilasakaravarthi@gmail.com>
2019-09-12 16:00:16 +0530

    Program

---
+++ src/org/tcs/test/Statements.java
@@ -0,0 +1,12 @@
+package org.tcs.test;
+
+public class Statements {
+public static void main(String[] args) {
+	for (int i = 1; i <=5; i++) {
+	for (int j = 1; j <=i; j++) {
+		System.out.print("*");
+	}
+	System.out.println();
+}
+}
+}
